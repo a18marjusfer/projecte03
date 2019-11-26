2029a28aff362eeb67beaefd0d847076f131fe42
Josue Zambrano <a18joszamper@inspedralbes.cat>
2019-11-26 16:18:20 +0100

    update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -5,3 +5,11 @@ ufw allow in "Apache Full"
 apt-get install -y mysql-server
 apt-get install -y php libapache2-mod-php php-mysql
 systemctl restart apache2
+mkdir /usr/share/adminer
+wget "http://www.adminer.org/latest.php" -O /usr/share/adminer/latest.php
+ln -s /usr/share/adminer/latest.php /usr/share/adminer/adminer.php
+echo "Alias /adminer.php /usr/share/adminer/adminer.php" | tee /etc/apache2/conf-available/adminer.conf
+a2enconf adminer.conf
+systemctl reload apache2
+
+

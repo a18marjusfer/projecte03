b62e9d0aba5062d821f04625f1058e2052d76903
Josue Zambrano <a18joszamper@inspedralbes.cat>
2019-11-26 15:54:33 +0100

    Update Vagranfile

--- lamp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-apt-get update
-apt-get install -y apache2
-ufw allow in "Apache Full"
-apt-get install -y mysql-server
-apt-get install -y php libapache2-mod-php php-mysql
-systemctl restart apache2
